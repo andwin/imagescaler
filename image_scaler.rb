72072018b5f23cdcb2c96be2901d28d9984f220f
Anders Windelhed <anders@windelhed.se>
2014-09-17 21:57:17 +0200

    Added method for getting output file name from source path

--- image_scaler.rb
+++ image_scaler.rb
@@ -1,2 +1,10 @@
 class ImageScaler
+  def self.output_file_name source_path
+    basename = File.basename(source_path)
+    extension = File.extname(basename).downcase.sub('.', '')
+
+    return basename if extension == 'jpg'
+
+    return File.basename(basename, File.extname(basename)) + "_" + extension + '.jpg'
+  end
 end
--- test/image_scaler_test.rb
+++ test/image_scaler_test.rb
@@ -2,4 +2,8 @@ require 'test_helper'
 require_relative '../image_scaler'
 
 class ImageScalerTest < ActiveSupport::TestCase
+  test "output_file_name should return the correct file name" do
+    assert_equal 'picture.jpg', ImageScaler.output_file_name('/path/to/picture.jpg')
+    assert_equal 'picture_cr2.jpg', ImageScaler.output_file_name('/path/to/picture.CR2')
+  end
 end
