7e85a22a1a375765c636bd1c3be5404db92e4751
Anders Windelhed <anders@windelhed.se>
2014-11-11 20:22:56 +0100

    Added method to resize images

--- image_scaler.rb
+++ image_scaler.rb
@@ -1,4 +1,15 @@
+require 'RMagick'
+include Magick
+
 class ImageScaler
+  def self.rescale_image source_path, output_dir, width, height
+    img = Image.read(source_path).first()
+    scaled_image = img.resize_to_fit(width, height)
+    output_path = File.join(output_dir, output_file_name(source_path))
+    scaled_image.write(output_path)
+    return output_path
+  end
+
   def self.output_file_name source_path
     basename = File.basename(source_path)
     extension = File.extname(basename).downcase.sub('.', '')
--- test/image_scaler_test.rb
+++ test/image_scaler_test.rb
@@ -1,7 +1,21 @@
+require 'tmpdir'
+require 'fastimage'
 require 'test_helper'
 require_relative '../image_scaler'
 
 class ImageScalerTest < ActiveSupport::TestCase
+  class ScalerImage < ActiveSupport::TestCase
+    test "scaling a portrait image should keep aspect ratio" do
+      Dir.mktmpdir do |dir|
+        output_file_path = ImageScaler.rescale_image('test/files/oops.jpg', dir, 640, 480)
+        width, heigth = FastImage.size(output_file_path)
+
+        assert_equal 358, width
+        assert_equal 480, heigth
+      end
+    end
+  end
+
   test "output_file_name should return the correct file name" do
     assert_equal 'picture.jpg', ImageScaler.output_file_name('/path/to/picture.jpg')
     assert_equal 'picture_cr2.jpg', ImageScaler.output_file_name('/path/to/picture.CR2')
