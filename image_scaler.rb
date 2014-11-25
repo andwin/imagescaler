85763d93b5c7dcf94c1b2766ba1d8a6cb2f2b3c9
Anders Windelhed <anders@windelhed.se>
2014-11-25 22:37:51 +0100

    Parsing command line arguments with slop

--- image_scaler.rb
+++ image_scaler.rb
@@ -1,7 +1,18 @@
 #!/usr/bin/env ruby
+require 'slop'
 require_relative 'lib/image_scaler'
+
+options = Slop.parse do
+  banner 'Usage: image_scaler.rb source_dir destination_dir [options]'
+
+  on 'w', 'width', 'Width of output images', argument: :required
+  on 'h', 'height', 'Height of output images', argument: :required
+end
 
 source_dir = ARGV[0]
 destination_dir = ARGV[1]
+
+p options[:height]
+p options[:width]
 
 ImageScaler.rescale_dir source_dir, destination_dir
