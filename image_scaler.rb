0153f077462512bd422c5608bc3837293255ed4d
Anders Windelhed <anders@windelhed.se>
2014-11-29 16:50:00 +0100

    Making sure source directory exists

--- image_scaler.rb
+++ image_scaler.rb
@@ -18,6 +18,11 @@ rescue Slop::Error => e
 end
 
 source_dir = ARGV[0]
+unless Dir.exists? source_dir
+  puts "Source directory '#{source_dir}' doesn't exist"
+  exit 1
+end
+
 destination_dir = ARGV[1]
 
 ImageScaler.rescale_dir source_dir, destination_dir, options[:height], options[:height]
